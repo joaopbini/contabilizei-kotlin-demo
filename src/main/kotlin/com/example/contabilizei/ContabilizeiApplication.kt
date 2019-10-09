6ca784f1c5ec92ce4e68bc5e4a5825491a072ddf
Joao Bini <joaopbini@gmail.com>
2019-10-09 00:26:00 -0300

    criando docker file e compose
    configurando bd

--- src/main/kotlin/com/example/contabilizei/ContabilizeiApplication.kt
+++ src/main/kotlin/com/example/contabilizei/ContabilizeiApplication.kt
@@ -2,10 +2,12 @@ package com.example.contabilizei
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class ContabilizeiApplication
 
 fun main(args: Array<String>) {
 	runApplication<ContabilizeiApplication>(*args)
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
 }
