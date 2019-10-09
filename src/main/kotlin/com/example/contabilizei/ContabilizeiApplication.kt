f9f99789ecc15ac5be1c4477cb2bfd4264d65396
Joao Bini <joaopbini@gmail.com>
2019-10-09 00:16:27 -0300

    init

---
+++ src/main/kotlin/com/example/contabilizei/ContabilizeiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.contabilizei
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContabilizeiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContabilizeiApplication>(*args)
+}
